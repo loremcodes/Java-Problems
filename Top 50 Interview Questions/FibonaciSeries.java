d75c9e5ef3a3f57956a8b94a023d856705d6b65a
sjprasad-bit <prasadjunghare2@gmail.com>
2022-12-03 16:26:29 +0530

    commit form vscode

--- Top 50 Interview Questions/FibonaciSeries.java
+++ Top 50 Interview Questions/FibonaciSeries.java
@@ -3,11 +3,25 @@
 import java.util.Scanner;
 
 public class FibonaciSeries {
-    static int Series (int integer){
+    static void Series (int integer){
+        // for frist two numbers
+		int num1 = 0, num2 = 1, num3;
+        System.out.println(num1 + " " + num2);
 
+        for (int index = 2; index < integer; ++index){
+            num3 = num2 + num1;
+            System.out.println(" " + num3);
+            num1 = num2;
+            num2 = num3;
+        }
     }
 
   public static void main(String arsg[]) {
+        Scanner sc = new Scanner(System.in);
+        int integer = sc.nextInt();
 
+        Series(integer);
+
+        sc.close();
   }
 }
