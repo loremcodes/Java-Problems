8cda8a6697e9ce585bbf7e0fe03559c20365cacd
prasad <86673407+sjprasad-bit@users.noreply.github.com>
2022-12-03 16:46:38 +0530

    string palinrome added

---
+++ Top 50 Interview Questions/StringPalindrome.java
@@ -0,0 +1,16 @@
+// Top 50 common interview programs -> 1. String Palindrome Program
+
+public class StringPalindrome {
+    static boolean isPalindrome(String str) {
+        String reverseStr = " ";
+        
+        for (int index = (str.length() -1); index >= 0; index--){
+            reverseStr = reverseStr + str.charAt(i)
+        }
+
+        return reverseStr;
+    }
+    public static void main(string args[]){
+        
+    }
+}
