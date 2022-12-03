7e8ac5b04b8ac99cbc825d9562b5f35100f6f702
sjprasad-bit <prasadjunghare2@gmail.com>
2022-12-03 13:55:08 +0530

    1 program added

---
+++ Top 50 Interview Questions/FibonaciSeries.java
@@ -0,0 +1,13 @@
+// Top 50 common interview programs -> 1. Fibonaci Series
+
+import java.util.Scanner;
+
+public class FibonaciSeries {
+    static int Series (int integer){
+
+    }
+
+  public static void main(String arsg[]) {
+
+  }
+}
